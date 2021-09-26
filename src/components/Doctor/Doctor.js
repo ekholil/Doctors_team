dc4dfef584b49ef5e23643feff621af253ae05c6
ekholil <kholilebrahim01@gmail.com>
2021-09-26 16:03:57 +0600

    final push

--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import {faUserPlus} from '@fortawesome/free-solid-svg-icons'
 import './Doctor.css'
 const Doctor = (props) => {
    
     const {first_name, last_name, age, email, country, salary, image} = props.doctor;
-    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
+    const addUser = <FontAwesomeIcon icon={faUserPlus} />
     return (
         <div className='col-md-4'>
             <div className='single-doctor shadow'>
@@ -15,7 +15,7 @@ const Doctor = (props) => {
                 <p>Age : {age}</p>
                 <p>Country : {country}</p>
                 <p>Salary : ${salary}</p>
-                <button onClick={() => props.handleClick(props.doctor)} className='button'>{cartIcon} Add to Team</button>
+                <button onClick={() => props.handleClick(props.doctor)} className='button'>{addUser} Add to Team</button>
                 <div>
                  
                 </div>
